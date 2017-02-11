9a6a73ee6a77d8165290d4c0ee8e738d0181da26
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-02-11 18:57:34 -0500

    Add PHP content header

--- Front-end/dologin.php
+++ Front-end/dologin.php
@@ -1,5 +1,7 @@
 <?php
 
+	header('Content-Type: application/json');
+
 	# Check Request Method
 	if ($_SERVER['REQUEST_METHOD'] != "POST") {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
@@ -27,22 +29,22 @@
 		]
 	];
 	
-	echo json_encode($ut);
+	# echo json_encode($ut);
 
-//	$curl = curl_init();
-//	curl_setopt_array($curl, [
-//	    CURLOPT_RETURNTRANSFER => 1,
-//	    CURLOPT_URL => 'https://web.njit.edu/~',
-//	    CURLOPT_USERAGENT => 'NJIT Auth Front-end',
-//	    CURLOPT_POST => 1,
-//	    CURLOPT_POSTFIELDS => [
-//	        "ucid" => $ucid,
-//	        "pass" => $pass
-//	    ]
-//	]);
-//	$resp = curl_exec($curl);
-//	var_dump($resp);
-//	# echo json_encode($resp);
-//	curl_close($curl);
+	$curl = curl_init();
+	curl_setopt_array($curl, [
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL => 'https://web.njit.edu/~ks492/searchform.php',
+	    CURLOPT_USERAGENT => 'NJIT Auth Front-end',
+	    CURLOPT_POST => 1,
+	    CURLOPT_POSTFIELDS => [
+	        "ucid" => $ucid,
+	        "pass" => $pass
+	    ]
+	]);
+	$resp = curl_exec($curl);
+	# var_dump($resp);
+	echo json_encode($resp);
+	curl_close($curl);
 
 ?>
